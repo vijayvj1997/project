4f8cd1a520a0e8f15e34a63112ecfb2188123738
vijayvj1997 <YoYochinnu@1997>
2018-03-05 17:03:29 +0530

    third change

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -9,7 +9,6 @@ public class test {
         Assert.assertTrue(main.prime(7));
         Assert.assertTrue(main.prime(11));
         Assert.assertTrue(main.prime(13));
-        Assert.assertTrue(main.prime(9494411));
         Assert.assertTrue(main.prime(17));
         Assert.assertTrue(main.prime(19));
         Assert.assertTrue(main.prime(23));
